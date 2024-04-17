c7e404238f9c5dacb48edb140be62e8c235d29a8
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-04-17 16:28:39 +0300

    Create Strings_Lesson_one.py

---
+++ testProject_1/Strings_Lesson_one.py
@@ -0,0 +1,8 @@
+testString = 'это моя строка'
+
+print(testString[0])
+print(testString[-1])
+print(testString[2:5])
+print(testString[::-1])     # print(''.join(reversed(testString)))
+print(len(testString))
+print('это новая строка ' + testString)
