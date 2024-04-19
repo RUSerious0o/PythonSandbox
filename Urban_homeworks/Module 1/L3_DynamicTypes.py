b35df8e0d2c383e78953a67137353f862c2f2916
Dmitriy K <d.o.konstantinov.0o@gmail.com>
2024-04-19 15:19:47 +0300

    Module 1, L3 HW added (DynamicVarTypes)

---
+++ Urban_homeworks/Module 1/L3_DynamicTypes.py
@@ -0,0 +1,11 @@
+name = 'Dmitriy'
+print(name)
+age = 35
+print(age)
+age += 1
+print(age)
+is_student = True
+print(is_student)
+
+art = 'a' if is_student else 'not'
+print(f'My name is {name}, and am {age} years old, and I\'m {art} student.')
