19abbd5b02ce2126c9806a9c9c9a0a3ab6e02a8e
Dmitriy K <d.o.konstantinov.0o@gmail.com>
2024-04-21 02:49:44 +0300

    Module 1, L6 HW added (dict && list practice)

---
+++ Urban_homeworks/Module 1/L6_DictSet.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'orange', 'coconut', 'banana', 'mango', 'lemon']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'kiwi'
+print(my_list)
+
+my_dict = {'apple': 'яблоко',
+           'banana': 'банан',
+           'orange': 'апельсин'}
+print('\n', my_dict)
+print(my_dict.get('apple'))
+my_dict['coconut'] = 'кокос'
+print(my_dict)
