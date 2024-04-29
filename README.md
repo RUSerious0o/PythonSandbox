ee74cc9902fdd5000ac82a760f4924058d8ac906
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-04-29 19:40:45 +0300

    Test.py modified during webinar # 2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PythonSandbox
  
+https://habr.com/ru/companies/skillfactory/articles/599599/ - Хабр про PyQt 6
--- Urban_homeworks/Test.py
+++ Urban_homeworks/Test.py
@@ -11,11 +11,28 @@
 # num2 = 27
 # print(type(num2), id(num2))
 
-dict_ = {'name': 'yura'}
-dict_.setdefault('name', 'typo')
-print('yura' in dict_, dict_)
+# dict_ = {'name': 'yura'}
+# dict_.setdefault('name', 'typo')
+# print('yura' in dict_, dict_)
+#
+# set_ = set([1, 2, 3, 4, 5])
+# print(set_)
+# set_2 = set([2, 3, 4, 6, 7])
+# print(set_ & set_2)
 
-set_ = set([1, 2, 3, 4, 5])
-print(set_)
-set_2 = set([2, 3, 4, 6, 7])
-print(set_ & set_2)
+# list_ = range(0, 15)
+# print(*list_)
+
+# Таблица умножения, второй вэбинар
+def print_mult_table(from_num, to_num):
+    def print_table_string(*args, mult=1):
+        for num in args:
+            print(num * mult, end='\t')
+        print()
+
+    rng = range(from_num, to_num)
+    for i in rng:
+        print_table_string(*rng, mult=i)
+
+
+print_mult_table(1, 17)
