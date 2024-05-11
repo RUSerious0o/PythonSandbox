d0b34557c4938db21f94ef73678abc6d2581d492
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-05-11 21:14:34 +0300

    Module 4, HW modules added

---
+++ Urban_homeworks/Module 4/L1_ImportModule.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print('Hello, world!')
+
+
+def say_hi():
+    print('Hi, world!')
---
+++ Urban_homeworks/Module 4/L1_Modules.py
@@ -0,0 +1,4 @@
+import L1_ImportModule as Hi_module
+
+Hi_module.say_hello()
+Hi_module.say_hi()
