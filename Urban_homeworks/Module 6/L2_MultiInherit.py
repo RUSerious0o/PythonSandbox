25552b4aeb71cad68b91ebfd6b7aa4ae690f1fe4
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-06-13 19:51:49 +0300

    Module 6, HW, multi inheritance fixed

--- Urban_homeworks/Module 6/L2_MultiInherit.py
+++ Urban_homeworks/Module 6/L2_MultiInherit.py
@@ -7,25 +7,23 @@ class Vehicle(object):
 
 
 class Car(object):
-    def __init__(self, horse_powers, price=10 ** 6):
+    def __init__(self, price=10 ** 6):
         self.__price = price
-        self.__horse_powers = horse_powers
 
     def get_horse_powers(self):
-        return self.__horse_powers
+        return 110
 
     def get_price(self):
         return self.__price
 
     def set_price(self, price):
         self.__price = price
-
+        
 
 class Nissan(Vehicle, Car):
-    def __init__(self, horse_powers, price=10 ** 6, vehicle_type=None):
+    def __init__(self, price=10 ** 6, vehicle_type=None):
         super().__init__(vehicle_type)
-        # Vehicle.__init__(self, vehicle_type)      # same result
-        Car.__init__(self, horse_powers,price)
+        Car.__init__(self, price)
 
     def __str__(self):
         return 'Vehicle type: {}, horse powers: {}, price: {}'.format(
@@ -37,11 +35,14 @@ class Nissan(Vehicle, Car):
     def __repr__(self):
         return self.__str__()
 
+    def get_horse_powers(self):
+        return 115
 
-nissans = [Nissan(172, 1.13 * 10 ** 6, 'car'),
-           Nissan(133, 0.9 * 10 ** 6),
-           Nissan(142)]
-print(nissans)
+
+cars = [Nissan(1.13 * 10 ** 6, 'car'),
+        Nissan(0.9 * 10 ** 6),
+        Nissan()]
+print(*cars, sep='\n')
 for i in range(3):
-    nissans[i].set_price(int((i + 1) * 0.7 * 10 ** 6))
-print(nissans)
+    cars[i].set_price(int((i + 1) * 0.7 * 10 ** 6))
+print(cars)
