439d592f17bbbd2982051c2ff24087986f6a899f
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-08-16 18:10:58 +0300

    Module 13 HW, telegram bot start. Answer practice

--- Bots/M13_L2_TestBot.py
+++ Bots/M13_L2_TestBot.py
@@ -11,11 +11,11 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print(f'Привет! Я бот помогающий твоему здоровью.')
+    await message.answer(f'Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def all_messages(message):
-    print(f'Введите команду /start, чтобы начать общение.')
+    await message.answer(f'Введите команду /start, чтобы начать общение.')
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp, skip_updates=True)
