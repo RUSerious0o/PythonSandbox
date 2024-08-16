a64e7e2886b2ccd2c412ac2f0a14a289d6508448
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-08-16 19:23:12 +0300

    Module 13 HW, telegram bot start. state machine practice (calories rule)

--- Bots/M13_L2_TestBot.py
+++ Bots/M13_L2_TestBot.py
@@ -1,21 +1,51 @@
-from pprint import pprint
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 from bot_token import __token
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+    data = {}
 
 
 bot = Bot(token=__token)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(commands=['start'])
-async def start(message):
-    await message.answer(f'Привет! Я бот помогающий твоему здоровью.')
 
-@dp.message_handler()
-async def all_messages(message):
-    await message.answer(f'Введите команду /start, чтобы начать общение.')
+@dp.message_handler(text=['Calories', 'calories', 'c'])
+async def set_age(message):
+    await message.answer('Введите свой возраст')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    UserState.data['age'] = message.text
+    await message.answer('Введите свой рост')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    UserState.data['growth'] = message.text
+    await message.answer('Введите свой вес')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    UserState.data['weight'] = message.text
+    await state.finish()
+
+    calories = (10 * float(UserState.data['weight']) +
+                6.25 * float(UserState.data['growth']) -
+                5 * float(UserState.data['age']) + 5)
+    await message.answer(f'Ваша норма калорий: {round(calories, 1)}')
+
 
 if __name__ == '__main__':
     executor.start_polling(dispatcher=dp, skip_updates=True)
