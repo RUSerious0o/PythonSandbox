99856c19556d2b0049578eb15104a5a05f81a203
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-09-01 21:19:15 +0300

    Module 14, bot refactor (aiogram 3.12 implementation)

---
+++ Urban_homeworks/Module 14/L3_TBotWithDB.py
@@ -0,0 +1,96 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from bot_token import __token
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+dp = Dispatcher(storage=MemoryStorage())
+
+reply_keyboard = ReplyKeyboardMarkup(keyboard=[
+    [
+        KeyboardButton(text='Рассчитать'),
+        KeyboardButton(text='Информация')
+    ]
+], resize_keyboard=True)
+
+inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories'),
+        InlineKeyboardButton(text='Формулы расчета', callback_data='formulas')
+    ]
+])
+
+
+@dp.message(CommandStart())
+async def start_bot(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(text='Привет! Я бот, помогающий твоему здоровью.', reply_markup=reply_keyboard)
+
+
+@dp.message(F.text == 'Рассчитать')
+async def main_menu(message: Message):
+    await message.answer(text='Выберите опцию', reply_markup=inline_keyboard)
+
+
+@dp.callback_query(F.data == 'formulas')
+async def get_formulas(call: CallbackQuery):
+    text = ('1. Упрощенный вариант формулы Миффлина-Сан Жеора:\n'
+            'для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;\n'
+            'для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.')
+    await call.message.answer(text=text)
+    await call.answer()
+
+
+@dp.callback_query(F.data == 'calories')
+async def set_age(call: CallbackQuery, state: FSMContext):
+    print(call.__class__)
+    await call.message.answer('Введите свой возраст')
+    await call.answer()
+    await state.set_state(UserState.age)
+
+
+@dp.message(UserState.age)
+async def set_growth(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост')
+    await state.set_state(UserState.growth)
+
+
+@dp.message(UserState.growth)
+async def set_weight(message: Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес')
+    await state.set_state(UserState.weight)
+
+
+@dp.message(UserState.weight)
+async def send_calories(message: Message, state: FSMContext):
+    data = await state.get_data()
+    await state.clear()
+
+    age = float(data['age'])
+    growth = float(data['growth'])
+    weight = float(message.text)
+    calories = 10 * weight + 6.25 * growth - 5 * age
+
+    await message.answer(f'Ваша норма калорий: {round(calories, 1)}')
+
+
+async def main():
+    bot = Bot(token=__token)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
