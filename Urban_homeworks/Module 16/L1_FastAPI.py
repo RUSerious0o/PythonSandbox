82de8a242e892230d28ca04307887bf759bb3485
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-09-18 22:39:43 +0300

    Module 16, FastAPI validation

--- Urban_homeworks/Module 16/L1_FastAPI.py
+++ Urban_homeworks/Module 16/L1_FastAPI.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
+
 import uvicorn
 
 app = FastAPI()
@@ -15,12 +17,13 @@ async def get_admin_page() -> str:
 
 
 @app.get('/user/{user_id}')
-async def get_user_page(user_id: int) -> str:
+async def get_user_page(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]) -> str:
     return f'Вы вошли как пользователь № {user_id}'
 
 
-@app.get('/user')
-async def get_user_page(username, age) -> str:
+@app.get('/user/{username}/{age}')
+async def get_user_page(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                        age: Annotated[int, Path(ge=18, le=120, description='Enter age')]) -> str:
     return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
 
 
