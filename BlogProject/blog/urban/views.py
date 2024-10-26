302a51315c1093089c009529603de7e973b9d5d6
RUSerious0o <83060758+RUSerious0o@users.noreply.github.com>
2024-10-26 16:16:25 +0300

    Module 19, test blog site, pagination

--- BlogProject/blog/urban/views.py
+++ BlogProject/blog/urban/views.py
@@ -1,11 +1,24 @@
+from django.http import HttpRequest
 from django.shortcuts import render
-# from django.core.paginator import Paginator
+from django.core.paginator import Paginator
 
 from .models import Author, Post
 
 
+posts_per_page = 2
+
+
 # Create your views here.
-def get_main_page(request):
+def get_main_page(request: HttpRequest):
+    global posts_per_page
+
+    posts = Post.objects.all().order_by('-creation_date')
+    if request.method == 'POST':
+        posts_per_page = request.POST.get('posts_per_page')
+    paginator = Paginator(posts, posts_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 'main.html', context={
-        'posts': Post.objects.all().order_by('-creation_date'),
+        'page_obj': page_obj,
     })
